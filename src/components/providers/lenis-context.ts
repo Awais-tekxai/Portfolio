c86b509b646e9c919dd3360482bda9be3e9b7adb
Awais-tekxai <Awais-tekxai@users.noreply.github.com>
2026-08-30 14:01:37 +0000

    deploy: 4565b048869b636aa75243a2e493f7ff3f9d3072

--- src/components/providers/lenis-context.ts
+++
@@ -1,6 +0,0 @@
-import Lenis from 'lenis'
-import { createContext } from 'react'
-
-export type LenisInstance = InstanceType<typeof Lenis>
-
-export const LenisContext = createContext<LenisInstance | null>(null)
